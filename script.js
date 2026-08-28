d672f90a3b4bb6120ef00b3ec0d0e19d80cd81ca
Kaveesha Senarathne <senarathnemalindi@gmail.com>
2026-08-28 13:49:55 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let key = "c32e2f58"
+
+function Search() {
+    // console.log("Searching...");
+    // console.log("Searching for movies....");
+
+    let movieName = document.getElementById("movie").value
+    console.log(movieName);
+
+    let url= "https://www.omdbapi.com/?t=" + movieName + "&apikey=" + key
+    console.log(url);
+
+    let httpRequest = new XMLHttpRequest()
+    httpRequest.open("GET", url)
+    httpRequest.responseType = "json"
+    httpRequest.send()
+
+    httpRequest.onload = function() {
+        console.log(httpRequest.response)
+        let movie = httpRequest.response
+
+        document.getElementById("title").innerHTML = movie.Title
+        document.getElementById("year").innerHTML = movie.Year
+        document.getElementById("poster").src = movie.Poster
+        document.getElementById("plot").innerHTML = movie.Plot
+    }
+}
